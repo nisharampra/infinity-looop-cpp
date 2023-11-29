b53c2a6fb5565b0bacfecaf5082e0b0f784b3943
Nisha Ramprasath <115043583+nisharampra@users.noreply.github.com>
2023-11-29 16:18:46 +0800

    Add files via upload

---
+++ infinity looop cpp/infinity looop cpp/main.cpp
@@ -0,0 +1,19 @@
+//
+//  main.cpp
+//  infinity looop cpp
+//
+//  Created by Nisha Ramprasath on 28/10/23.
+//
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int i = 0;
+    for (;;) {
+        cout << " Hello";
+        i++;
+    }
+
+    return 0;
+}
